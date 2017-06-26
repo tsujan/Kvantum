afb29773a682d06ca80499047c396e72e035da15
Craig Drummond <craig.p.drummond@gmail.com>
2017-06-26 22:36:49 +0100

    Fix QtCreator segfault. In dreawPrimitiveElement(PE_Frame) need to check valid widgert before using widget->inherits()

--- Kvantum/style/Kvantum.cpp
+++ Kvantum/style/Kvantum.cpp
@@ -3742,7 +3742,7 @@ void Style::drawPrimitive(PrimitiveElement element,
           }
         }
 
-        if (widget->inherits("QComboBoxPrivateContainer")
+        if (widget && widget->inherits("QComboBoxPrivateContainer")
             && tspec_.combo_menu)
         {
           painter->fillRect(option->rect, option->palette.color(QPalette::Window));
