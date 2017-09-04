431f43dd5cb24013a73bfd2d04c47a855566ab6f
Tsu Jan <tsujan2000@gmail.com>
2017-09-04 14:14:10 +0430

    Clear button for the theme combo of Kvantum manager

--- Kvantum/kvantummanager/combobox.h
+++ Kvantum/kvantummanager/combobox.h
@@ -3,6 +3,9 @@
 
 #include <QComboBox>
 #include <QCompleter>
+#if QT_VERSION >= 0x050200
+#include <QLineEdit>
+#endif
 
 namespace KvManager {
 
@@ -16,6 +19,7 @@ public:
         completer()->setCompletionMode (QCompleter::PopupCompletion);
 #if QT_VERSION >= 0x050200
         completer()->setFilterMode (Qt::MatchContains);
+        lineEdit()->setClearButtonEnabled (true);
 #endif
         connect (this, SIGNAL (currentIndexChanged (const QString&)),
                  this, SLOT (textChangedSlot (const QString&)));
@@ -34,17 +38,28 @@ private slots:
 protected:
     void focusOutEvent (QFocusEvent *e)
     {
-        /* return to the current index on focusing out
-           if no item starts with the current text */
+        if (currentText().isEmpty())
+        {
+            if (currentIndex() > -1)
+                setCurrentIndex (currentIndex());
+        }
+        else
+        {
+            /* return to the current index on focusing out
+               if no item starts with the current text */
 #if QT_VERSION >= 0x050200
-        int indx = findText (currentText(), Qt::MatchContains);
+            int indx = findText (currentText(), Qt::MatchContains);
 #else
-        int indx = findText (currentText(), Qt::MatchStartsWith);
+            int indx = findText (currentText(), Qt::MatchStartsWith);
 #endif
-        if (indx == -1)
-            setCurrentIndex (currentIndex());
-        else // needed because of the popup
-            setCurrentIndex (indx);
+            if (indx == -1)
+            {
+                if (currentIndex() > -1)
+                    setCurrentIndex (currentIndex());
+            }
+            else // needed because of the popup
+                setCurrentIndex (indx);
+        }
         QComboBox::focusOutEvent (e);
     }
 
