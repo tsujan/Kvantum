64c777b248b3bf531b1589e6d19daa043a199037
Tsu Jan <tsujan2000@gmail.com>
2018-02-01 21:55:42 +0330

    A 1-px more padding between label and focus rect
    Also theme tweaks.

--- Kvantum/style/Kvantum.cpp
+++ Kvantum/style/Kvantum.cpp
@@ -8029,9 +8029,9 @@ void Style::drawControl(ControlElement element,
           if (fspec.expansion > 0)
           {
             if (rtl)
-              fropt.rect = labelRect(r, fspec, lspec).adjusted(closable ? -lspec.tispace : -1, -1, 1, 1);
+              fropt.rect = labelRect(r, fspec, lspec).adjusted(closable ? -lspec.tispace : -2, -2, 2, 2);
             else
-              fropt.rect = labelRect(r, fspec, lspec).adjusted(-1, -1, closable ? lspec.tispace : 1, 1);
+              fropt.rect = labelRect(r, fspec, lspec).adjusted(-2, -2, closable ? lspec.tispace : 2, 2);
           }
           else
           {
@@ -9797,7 +9797,7 @@ void Style::drawControl(ControlElement element,
           QStyleOptionFocusRect fropt;
           fropt.QStyleOption::operator=(*opt);
           if (fspec.expansion > 0)
-            fropt.rect = labelRect(option->rect, fspec, lspec).adjusted(-1,-1,1,1);
+            fropt.rect = labelRect(option->rect, fspec, lspec).adjusted(-2,-2,2,2);
           else
             fropt.rect = interiorRect(option->rect, fspec);
           drawPrimitive(PE_FrameFocusRect, &fropt, painter, widget);
@@ -10704,7 +10704,7 @@ void Style::drawComplexControl(ComplexControl control,
           QStyleOptionFocusRect fropt;
           fropt.QStyleOption::operator=(*opt);
           if (fspec.expansion > 0)
-            fropt.rect = labelRect(opt->rect, fspec, lspec).adjusted(-1,-1,1,1);
+            fropt.rect = labelRect(opt->rect, fspec, lspec).adjusted(-2,-2,2,2);
           else
             fropt.rect = interiorRect(opt->rect, fspec);
           drawPrimitive(PE_FrameFocusRect, &fropt, painter, widget);
@@ -11210,7 +11210,7 @@ void Style::drawComplexControl(ComplexControl control,
               QStyleOptionFocusRect fropt;
               fropt.QStyleOption::operator=(*opt);
               if (fspec.expansion > 0)
-                fropt.rect = labelRect(r, fspec, lspec).adjusted(-1,-1,1,1);
+                fropt.rect = labelRect(r, fspec, lspec).adjusted(-2,-2,2,2);
               else
                 fropt.rect = interiorRect(r, fspec);
               drawPrimitive(PE_FrameFocusRect, &fropt, painter, widget);
