8bc0b99a320688cfccaf9e6527504ab4a4913476
roxfr <roxfr@outlook.fr>
2019-03-01 20:56:55 +0000

    Translated using Weblate (French)
    Currently translated at 3.1% (8 of 257 strings)
    
    Translation: tsujan/Kvantum (Manager)
    Translate-URL: https://weblate.lxqt.org/projects/tsujan/kvantum-manager/fr/

--- Kvantum/kvantummanager/data/translations/kvantummanager_fr.ts
+++ Kvantum/kvantummanager/data/translations/kvantummanager_fr.ts
@@ -7,44 +7,46 @@
         <location filename="../../kvantummanager.ui" line="12"/>
         <location filename="../../KvantumManager.cpp" line="2606"/>
         <source>Kvantum Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Kvantum Manager</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="38"/>
         <source>Install/Update Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Installation/mise à jour du thème</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="44"/>
         <source>&lt;center&gt;&lt;b&gt;&lt;i&gt;Kvantum&lt;/i&gt;&lt;/b&gt; comes with many themes but external themes can also be installed for being used by it. Here you could choose a theme folder and then press the button below to install it in your Home.&lt;/center&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;center&gt;&lt;b&gt;&lt;i&gt;Kvantum&lt;/i&gt;&lt;/b&gt; contient de nombreux thèmes, mais des thèmes externes peuvent également être installés pour pouvoir être utilisés par celui-ci. Ici, vous pouvez choisir un dossier de thèmes, puis appuyer sur le bouton ci-dessous pour l’installer dans votre accueil.&lt;/center&gt;</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="76"/>
         <source>Select a Kvantum
 theme folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Sélectionner un Kvantum
+dossier de thème</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="113"/>
         <source>Add this theme to the list of themes that
 Kvantum can use in addition to its own themes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajouter ce thème à la liste des thèmes que
+Kvantum peut utiliser en plus de ses propres thèmes.</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="117"/>
         <source>Install this theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Installer ce thème</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="138"/>
         <source>Change/Delete Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Changer/Supprimer le thème</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="144"/>
         <source>Select a theme:</source>
-        <translation type="unfinished"></translation>
+        <translation>Sélectionner un thème :</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="191"/>
