83f70a84fc1b4003e7d5b3c55986a47331be420c
Itay Alush <itay082007@gmail.com>
2021-01-11 07:52:03 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 11.1% (30 of 269 strings)
    
    Translation: Tsujan/Kvantum Manager
    Translate-URL: https://translate.lxqt-project.org/projects/tsujan/kvantum-manager/he/

--- Kvantum/kvantummanager/data/translations/kvantummanager_he.ts
+++ Kvantum/kvantummanager/data/translations/kvantummanager_he.ts
@@ -31,34 +31,34 @@
         <source>Select a Kvantum
 theme folder</source>
         <translation>נא לבחור תיקיית ערכות
-עיצוב של Kvantum</translation>
+נושא של Kvantum</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="113"/>
         <source>Add this theme to the list of themes that
 Kvantum can use in addition to its own themes.</source>
-        <translation>להוסיף את ערכת העיצוב הזאת לרשימת ערכת העיצוב
-בהן יכול Kvantum להשתמש במקום ערכות העיצוב שלו.</translation>
+        <translation>הוסף את ערכת העיצוב הזאת לרשימת ערכת הנושא
+בהן Kvantum יכול להשתמש בנוסף לערכות הנושא שלו.</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="117"/>
         <source>Install this theme</source>
-        <translation>התקנת ערכת העיצוב הזו</translation>
+        <translation>התקן את ערכת הנושא הזו</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="138"/>
         <source>Change/Delete Theme</source>
-        <translation>החלפת/מחיקת ערכת עיצוב</translation>
+        <translation>החלף/מחק ערכת נושא</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="144"/>
         <source>Select a theme:</source>
-        <translation>נא לבחור ערכת עיצוב:</translation>
+        <translation>נא לבחור ערכת נושא:</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="191"/>
         <source>Delete this theme</source>
-        <translation>מחיקת ערכת העיצוב הזו</translation>
+        <translation>מחיקת ערכת הנושא הזו</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="198"/>
@@ -66,17 +66,18 @@ Kvantum can use in addition to its own themes.</source>
 
 A disabled button means that
 the theme is already used.</source>
-        <translation type="unfinished"></translation>
+        <translation>גרום לKvantum להשתמש בערכת הנושא הזו
+כפתור חסום אומר שערכת הנושא הזו כבר נמצאת בשימוש.</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="204"/>
         <source>Use this theme</source>
-        <translation type="unfinished"></translation>
+        <translation>השתמש בערכת הנושא הזו</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="257"/>
         <source>Configure Active Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>ערוך ערכת נושא פעילה</translation>
     </message>
     <message>
         <location filename="../../kvantummanager.ui" line="321"/>
